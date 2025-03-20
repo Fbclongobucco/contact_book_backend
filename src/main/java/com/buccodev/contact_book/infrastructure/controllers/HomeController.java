c71bf06ffad8d6115e7a613bf743d10dc2f49b4f
longobuccodev <longobuccofbc@gmail.com>
2025-03-20 20:19:44 -0300

    create home controller api

--- src/main/java/com/buccodev/contact_book/infrastructure/controllers/HomeController.java
+++ src/main/java/com/buccodev/contact_book/infrastructure/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package com.buccodev.contact_book.infrastructure.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/api/contact-book")
+    public String home() {
+        return "redirect:/index.html";
+    }
 }

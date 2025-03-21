bc69e33ce46b51d83c6ab83e14fc2c286f77b1e6
longobuccodev <longobuccofbc@gmail.com>
2025-03-21 16:36:00 -0300

    adding the use case of deleting all contacts of a user.

--- src/main/java/com/buccodev/contact_book/application/usecases/user_usecases/DeleteUser.java
+++ src/main/java/com/buccodev/contact_book/application/usecases/user_usecases/DeleteUser.java
@@ -3,4 +3,6 @@ package com.buccodev.contact_book.application.usecases.user_usecases;
 public interface DeleteUser {
 
     void deleteUserById(Long id);
+
+    void deleteAllUsersByUser(Long userId);
 }

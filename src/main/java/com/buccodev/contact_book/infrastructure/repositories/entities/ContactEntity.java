5816e87da7a5416daa81dabd7692cf9ee18a9cc7
longobuccodev <longobuccofbc@gmail.com>
2025-03-26 15:53:15 -0300

    Creation of the infrastructure layer classes with JPA mapping.

--- src/main/java/com/buccodev/contact_book/infrastructure/repositories/entities/ContactEntity.java
+++ src/main/java/com/buccodev/contact_book/infrastructure/repositories/entities/ContactEntity.java
@@ -1,4 +1,78 @@
 package com.buccodev.contact_book.infrastructure.repositories.entities;
 
+import com.buccodev.contact_book.core.domain.Contact;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "contact_tb")
 public class ContactEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required!")
+    @Size(min = 3, max = 150, message = "Name must be at least 3 characters and less than 150!")
+    private String name;
+
+    @NotBlank(message = "Number is required!")
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Invalid phone number!")
+    private String number;
+
+    private String contactPhoto;
+
+    @ManyToOne
+    @JoinColumn(name = "userEntity_id")
+    private UserEntity userEntity;
+
+    public ContactEntity(Contact contact) {
+        this.id = contact.getId();
+        this.name = contact.getName();
+        this.number = contact.getNumber();
+        this.contactPhoto = contact.getContactPhoto();
+        this.userEntity = new UserEntity(contact.getUser());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getContactPhoto() {
+        return contactPhoto;
+    }
+
+    public void setContactPhoto(String contactPhoto) {
+        this.contactPhoto = contactPhoto;
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
 }
--- src/main/java/com/buccodev/contact_book/infrastructure/repositories/entities/UserEntity.java
+++ src/main/java/com/buccodev/contact_book/infrastructure/repositories/entities/UserEntity.java
@@ -1,4 +1,79 @@
 package com.buccodev.contact_book.infrastructure.repositories.entities;
 
+
+import com.buccodev.contact_book.core.domain.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user_tb")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Name is required!")
+    @NotBlank(message = "Name is required!")
+    private String name;
+
+    @Email(message = "Email is not valid!")
+    @NotBlank(message = "Email is required!")
+    private String email;
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 6, message = "Password must be at least 6 characters!")
+    private String password;
+
+    @OneToMany(mappedBy = "userEntity")
+    private final Set<ContactEntity> contacts = new HashSet<>();
+
+    public UserEntity(User user){
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<ContactEntity> getContacts() {
+        return contacts;
+    }
 }

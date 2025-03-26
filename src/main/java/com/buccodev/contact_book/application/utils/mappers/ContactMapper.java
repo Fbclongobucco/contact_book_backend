c1f9d9f7a12f4c59c2bbe99e24a9c5587701e894
longobuccodev <longobuccofbc@gmail.com>
2025-03-26 19:07:10 -0300

    Creation of Entity/DTOS Mapters of the Application

--- src/main/java/com/buccodev/contact_book/application/utils/mappers/ContactMapper.java
+++ src/main/java/com/buccodev/contact_book/application/utils/mappers/ContactMapper.java
@@ -1,4 +1,21 @@
 package com.buccodev.contact_book.application.utils.mappers;
 
+import com.buccodev.contact_book.application.utils.dtos.contact_dto.ContactRequestDto;
+import com.buccodev.contact_book.application.utils.dtos.contact_dto.ContactResponseDto;
+import com.buccodev.contact_book.core.domain.Contact;
+
 public class ContactMapper {
+
+    public static Contact contactDtoToContact(ContactRequestDto contactRequestDto) {
+        var contact = new Contact(null, contactRequestDto.name(), contactRequestDto.number(), null);
+        contact.setContactPhoto(contact.getContactPhoto());
+        return contact;
+    }
+
+    public static ContactResponseDto fromContactToContactResponseDto(Contact contact) {
+        return new ContactResponseDto(contact.getId(),
+                contact.getName(),
+                contact.getNumber(),
+                contact.getContactPhoto());
+    }
 }
--- src/main/java/com/buccodev/contact_book/application/utils/mappers/UserMapper.java
+++ src/main/java/com/buccodev/contact_book/application/utils/mappers/UserMapper.java
@@ -1,4 +1,25 @@
 package com.buccodev.contact_book.application.utils.mappers;
 
+import com.buccodev.contact_book.application.utils.dtos.user_dtos.UserRequestDto;
+import com.buccodev.contact_book.application.utils.dtos.user_dtos.UserResponseDto;
+import com.buccodev.contact_book.core.domain.User;
+
 public class UserMapper {
+
+    public static User fromRequestDtoToUser(UserRequestDto userRequestDto) {
+        return new User(
+                null,
+                userRequestDto.name(),
+                userRequestDto.email(),
+                userRequestDto.password()
+        );
+    }
+
+    public static UserResponseDto fromUserToResponseDto(User user) {
+        return new UserResponseDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
+    }
 }

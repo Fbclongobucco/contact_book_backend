538faa0e7eefdb83fd7cedf37f04e94d61aed8c2
longobuccodev <longobuccofbc@gmail.com>
2025-03-26 15:57:25 -0300

    Gateway adjustments of the Getuser class

--- src/main/java/com/buccodev/contact_book/infrastructure/services/user_service/GetUserService.java
+++ src/main/java/com/buccodev/contact_book/infrastructure/services/user_service/GetUserService.java
@@ -1,4 +1,41 @@
 package com.buccodev.contact_book.infrastructure.services.user_service;
 
-public class GetUserService {
+import com.buccodev.contact_book.application.usecases.user_usecases.GetUser;
+import com.buccodev.contact_book.core.domain.User;
+import com.buccodev.contact_book.infrastructure.repositories.entities_respository.UserEntityRepository;
+
+import java.util.List;
+
+public class GetUserService implements GetUser {
+
+    private final UserEntityRepository repository;
+
+    public GetUserService(UserEntityRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User getUserByName(String name) {
+        return null;
+    }
+
+    @Override
+    public User login(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers(Integer page, Integer size) {
+        return List.of();
+    }
 }
